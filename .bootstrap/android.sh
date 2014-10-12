a3b1fd5e8de112a03c3d11ef7721db3aa0aa5720
Jonas Eckerström <jonaseck@gmail.com>
2014-10-12 12:55:26 +0200

    Split the android.sh scripts into different sections
    Also fixed some obvious issues and added some changes to
    avoid unnecessary downloading when reprovisioning

--- .bootstrap/android.sh
+++ .bootstrap/android.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
+android_sdk="android-sdk_r23.0.2-linux.tgz"
+android_sdk_url="http://dl.google.com/android/${android_sdk}"
+intel_image="sysimg_x86-19_r01.zip"
+intel_image_url="http://download-software.intel.com/sites/landingpage/android/${intel_image}"
+android_device="Nexus S"
+
 if [ ! -d "${HOME}/android-sdk-linux/" ] ; then
 	
-	sudo apt-get -y install openjdk-7-jdk ant expect unzip
+	sudo apt-get -y install openjdk-7-jdk ant
 
-	android_sdk="android-sdk_r23.0.2-linux.tgz"
-	android_sdk_url="http://dl.google.com/android/${android_sdk}"
-	intel_image="sysimg_x86-19_r01.zip"
-	intel_image_url="http://download-software.intel.com/sites/landingpage/android/${intel_image}"
-	android_device="Nexus S"
-
-	echo 'export ANDROID_SDK_HOME=/vagrant/.android' >> ${HOME}/.bash_profile
+	echo 'export ANDROID_SDK_HOME=/vagrant/' >> ${HOME}/.bash_profile
 	echo "export ANDROID_HOME=${HOME}/android-sdk-linux" >> ${HOME}/.bash_profile
 	echo 'export PATH=${PATH}:${ANDROID_HOME}/platform-tools:${ANDROID_HOME}/tools' >> ${HOME}/.bash_profile
 	echo 'export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386' >> ${HOME}/.bash_profile
@@ -25,20 +25,28 @@ if [ ! -d "${HOME}/android-sdk-linux/" ] ; then
 	fi
 
 	tar -xvzf /vagrant/.download/${android_sdk}
+fi
+
+if [ ! -d  "${ANDROID_HOME}/platforms/android-19" ] ; then
+	sudo apt-get -y install expect
 
 	expect -c '
 	set timeout -1   ;
 	spawn android update sdk -u --all --filter platform-tool,android-19,build-tools-19.1.0
 	expect { 
-    	"Do you accept the license" { exp_send "y\r" ; exp_continue }
-    	eof
-	}
-	'
+	"Do you accept the license" { exp_send "y\r" ; exp_continue }
+	eof
+}
+'
+fi
+
+if [ ! -d "${ANDROID_HOME}/system-images/android-19/default/x86" ] ; then
+	sudo apt-get -y install unzip
 
 	#download intel x86 android image api level 19
 	if [ ! -f "/vagrant/.download/${intel_image}" ] ; then
 		echo "downloading ${intel_image_url}"
-		wget --quiet intel_image_url -P /vagrant/.download/
+		wget --quiet ${intel_image_url} -P /vagrant/.download/
 	fi
 
 	mkdir -vp ${ANDROID_HOME}/system-images/android-19/default/
